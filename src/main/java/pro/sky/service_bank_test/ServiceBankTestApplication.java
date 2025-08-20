55fb1bf3db76ac0ead9f39b78b0c7b1075698a8d
https://github.com/enot-therock/if_else.git <enot.therock@gmail..com>
2025-08-20 19:34:13 +0300

    let`s start

---
+++ src/main/java/pro/sky/service_bank_test/ServiceBankTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.service_bank_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBankTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/service_bank_test/ServiceBankTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.service_bank_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBankTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
