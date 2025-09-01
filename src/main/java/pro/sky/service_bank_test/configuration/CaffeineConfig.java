f6ce4b2915cf738530a4995feee46753dfaa7a6d
https://github.com/enot-therock/if_else.git <enot.therock@gmail..com>
2025-09-01 17:53:07 +0300

    step number 3.1

--- src/main/java/pro/sky/service_bank_test/configuration/CaffeineConfig.java
+++ src/main/java/pro/sky/service_bank_test/configuration/CaffeineConfig.java
@@ -2,6 +2,7 @@ package pro.sky.service_bank_test.configuration;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,18 +11,24 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class CaffeineConfig {
 
+    @Value("${caffeine.maximumSize}")
+    private int maximumSize;
+
+    @Value("${caffeine.endAccess}")
+    private int endAccess;
+
     @Bean
     public Cache<String, Object> caffeineCache() {
         return Caffeine.newBuilder()
-                .maximumSize(1000)
-                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .maximumSize(maximumSize)
+                .expireAfterAccess(endAccess, TimeUnit.MINUTES)
                 .build();
     }
 
     @Bean
     public Caffeine caffeine() {
         return Caffeine.newBuilder()
-                .maximumSize(1000)
-                .expireAfterAccess(10, TimeUnit.MINUTES);
+                .maximumSize(maximumSize)
+                .expireAfterAccess(endAccess, TimeUnit.MINUTES);
     }
 }
--- src/main/java/pro/sky/service_bank_test/configuration/RecommendationByRuleDataSourceConfiguration.java
+++ src/main/java/pro/sky/service_bank_test/configuration/RecommendationByRuleDataSourceConfiguration.java
@@ -1,5 +1,6 @@
 package pro.sky.service_bank_test.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +11,22 @@ import javax.sql.DataSource;
 @Configuration
 public class RecommendationByRuleDataSourceConfiguration {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Primary
     @Bean(name = "defaultDataSource")
     public DataSource defaultDataSource(DataSourceProperties properties) {
         return properties.initializeDataSourceBuilder()
-                .url("jdbc:postgresql://localhost:5432/service_bank")
-                .username("users")
-                .password("123456")
+                .url(url)
+                .username(userName)
+                .password(password)
                 .build();
     }
 
